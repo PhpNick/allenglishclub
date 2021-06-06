6768e771b84dca47eaf541b726cb3a86d39abf09
Nick Starkov <phpnick.ru@gmail.com>
2021-06-06 21:19:56 +0500

    Second

--- accomplishment/script.js
+++ accomplishment/script.js
@@ -13,7 +13,7 @@ Vue.component('button-bre-play', {
   },  
   template: `
   	<button type="button" @click="play" class="btn btn-primary btn-sm mt-1">
-  	BrE <img src="/img/listen-icon.svg" alt="">
+  	BrE <img src="/accomplishment/img/listen-icon.svg" alt="">
   	</button>
   `
 });
@@ -33,7 +33,7 @@ Vue.component('button-ame-play', {
   },  
   template: `
   	<button type="button" @click="play" class="btn btn-danger btn-sm mt-1">
-  	AmE <img src="/img/listen-icon.svg" alt="">
+  	AmE <img src="/accomplishment/img/listen-icon.svg" alt="">
   	</button>
   `
 });
