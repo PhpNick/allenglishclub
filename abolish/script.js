330b3c1bb3db612253cd0319df3b690a95361477
Nick Starkov <phpnick.ru@gmail.com>
2021-06-07 21:29:09 +0500

    abolish

---
+++ abolish/script.js
@@ -0,0 +1,42 @@
+Vue.component('button-bre-play', {
+  props: ['href'],
+  methods: {
+    play() {
+    	var loader = document.getElementById('loader');
+    	loader.style.visibility = "visible";
+    	var audio = new Audio(this.href);
+		audio.addEventListener("canplaythrough", event => {
+		  loader.style.visibility = "hidden";
+		  audio.play();
+		});
+    },
+  },  
+  template: `
+  	<button type="button" @click="play" class="btn btn-primary btn-sm mt-1">
+  	BrE <img src="/allenglishclub/abolish/img/listen-icon.svg" alt="">
+  	</button>
+  `
+});
+
+Vue.component('button-ame-play', {
+  props: ['href'],
+  methods: {
+    play() {
+    	var loader = document.getElementById('loader');
+    	loader.style.visibility = "visible";
+    	var audio = new Audio(this.href);
+		audio.addEventListener("canplaythrough", event => {
+		  loader.style.visibility = "hidden";
+		  audio.play();
+		});
+    },
+  },  
+  template: `
+  	<button type="button" @click="play" class="btn btn-danger btn-sm mt-1">
+  	AmE <img src="/allenglishclub/abolish/img/listen-icon.svg" alt="">
+  	</button>
+  `
+});
+
+
+new Vue({ el: '#app' });
